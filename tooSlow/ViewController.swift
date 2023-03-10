43039df104c63c59ac6b2efaf0af4903bce24cd8
maxxxondagon <maxxondagon@gmail.com>
2023-03-10 12:08:36 +0500

    Initial Commit

---
+++ tooSlow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tooSlow
+//
+//  Created by Maxim Soloboev on 10.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
